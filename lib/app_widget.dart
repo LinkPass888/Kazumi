d2785812af2374186166cedbe8635a4d90545965
Predidit <34627277+Predidit@users.noreply.github.com>
2026-09-09 18:43:40 +0800

    feat(player): add network aware low memory mode with manual overrides
    (cherry picked from commit 22f9ee13a93dbb64f0d300928990f7c38e1237d6)

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -7,6 +7,7 @@ import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:kazumi/services/storage/storage.dart';
 import 'package:tray_manager/tray_manager.dart';
 import 'package:kazumi/services/logging/logger.dart';
+import 'package:kazumi/services/network/metered_network_service.dart';
 import 'package:window_manager/window_manager.dart';
 import 'package:kazumi/bean/dialog/dialog_helper.dart';
 import 'package:kazumi/bean/settings/theme_provider.dart';
@@ -268,6 +269,7 @@ class _AppWidgetState extends State<AppWidget>
     } else if (state == AppLifecycleState.resumed) {
       KazumiLogger()
           .i("AppLifecycleState.resumed: Application moved to foreground");
+      await MeteredNetworkService.refresh();
     } else if (state == AppLifecycleState.inactive) {
       KazumiLogger().i("AppLifecycleState.inactive: Application is inactive");
     }
