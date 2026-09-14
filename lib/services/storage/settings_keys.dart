bd9c110f0168d3fe1599822a0f9b5d2266442cca
Predidit <34627277+Predidit@users.noreply.github.com>
2026-09-14 21:44:11 +0800

    feat(sync): add webdav sync for danmaku block rules
    (cherry picked from commit dc5bd86049d1514cfde7ac121b64f3a3aa81548c)

--- lib/services/storage/settings_keys.dart
+++ lib/services/storage/settings_keys.dart
@@ -288,6 +288,26 @@ class SettingsKeys {
     false,
     group: SettingGroup.webdav,
   );
+  static const webDavEnableDanmakuShield = SettingKey<bool>(
+    'webDavEnableDanmakuShield',
+    false,
+    group: SettingGroup.webdav,
+  );
+  static const danmakuShieldSyncDeviceId = SettingKey<String>(
+    'danmakuShieldSyncDeviceId',
+    '',
+    group: SettingGroup.sync,
+  );
+  static const danmakuShieldSyncState = SettingKey<String>(
+    'danmakuShieldSyncState',
+    '',
+    group: SettingGroup.sync,
+  );
+  static const danmakuShieldSyncCorruptState = SettingKey<String>(
+    'danmakuShieldSyncCorruptState',
+    '',
+    group: SettingGroup.sync,
+  );
   static const webDavURL = SettingKey<String>(
     _SettingBoxKey.webDavURL,
     '',
@@ -577,6 +597,10 @@ class SettingsKeys {
     webDavEnable,
     webDavEnableHistory,
     webDavEnableCollect,
+    webDavEnableDanmakuShield,
+    danmakuShieldSyncDeviceId,
+    danmakuShieldSyncState,
+    danmakuShieldSyncCorruptState,
     webDavURL,
     webDavUsername,
     webDavPassword,
